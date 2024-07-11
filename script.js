f6638e8faeae800148c7b0fce5cdb29dfa2a8e46
nandanarifqiirfansyah <nandanarifqiirfansyah@gmail.com>
2024-07-11 08:40:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(window).on("scroll", function(){
+	if($(window).scrollTop() > 50){
+		$("nav").addClass("active");
+	} else {
+		$("nav").removeClass("active");
+	}
+});
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
